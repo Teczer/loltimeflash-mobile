176364e245412337bf6528e8398516128230442c
Teczer <mehdi.hattou1@gmail.com>
2026-02-02 13:38:13 +0100

    :sparkles: add new item assets and sync script; update package.json with sync:items command

--- features/lanegap/components/item-spike-item.component.tsx
+++ features/lanegap/components/item-spike-item.component.tsx
@@ -1,6 +1,6 @@
 import { Image, Text, View } from 'react-native'
 
-import config from '@/lib/config'
+import { getItemIcon } from '@/assets/items'
 
 import type { IItemSpike } from '../types'
 
@@ -8,19 +8,15 @@ interface IItemSpikeItemProps {
   spike: IItemSpike
 }
 
-const DDRAGON_BASE = 'https://ddragon.leagueoflegends.com'
-
 export const ItemSpikeItem = ({ spike }: IItemSpikeItemProps) => {
-  const itemIconUrl = `${DDRAGON_BASE}/cdn/${config.patchVersion}/img/item/${spike.itemId}.png`
+  const itemIcon = getItemIcon(spike.itemId)
 
   return (
     <View className="flex-row items-center gap-3 rounded-lg bg-white/5 p-3">
       <View className="size-10 overflow-hidden rounded-lg border border-white/10">
-        <Image
-          source={{ uri: itemIconUrl }}
-          className="size-full"
-          resizeMode="cover"
-        />
+        {itemIcon && (
+          <Image source={itemIcon} className="size-full" resizeMode="cover" />
+        )}
       </View>
 
       <Text className="flex-1 text-sm leading-relaxed text-white/70">
